2f62ecb2cd93050b6d2f8080b36d3d3aa0338fa7
Yash Agrawal <yagrawl2@gmail.com>
2017-11-17 20:41:21 -0600

    adding skeleton

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDlXpYAD_nr6RCTNZZWyRkLyZNBOsLZI8I';
+
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
