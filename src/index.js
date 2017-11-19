af9f1c26d45ad24b5c26c4e6c0f3d837876422b0
Yash Agrawal <yagrawl2@gmail.com>
2017-11-18 20:12:35 -0600

    search works + throttling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -12,19 +13,32 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {videos: []};
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
 
-		YTSearch({key: API_KEY, term: 'Cage The Elephant'}, videos => {
-			this.setState({videos});
+		this.videoSearch('Rachel McAdams');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, videos => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 
 	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
 		return (
 			<div>
-				<SearchBar />
-				<VideoDetail video = {this.state.videos[0]}/>
-				<VideoList videos = {this.state.videos} />
+				<SearchBar onSearchTermChange = {videoSearch} />
+				<VideoDetail video = {this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+					videos = {this.state.videos} />
 			</div>
 		);
 	}
